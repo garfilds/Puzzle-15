df98e7062e2b6b9dc723197bf60e88b4a50ddf64
Arvils <arvils.apsitis@gmail.com>
2020-06-27 16:23:22 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuzzleFifteen
+{
+    public partial class PuzzleArea : Form
+    {
+        public PuzzleArea()
+        {
+            InitializeComponent();
+            InitializePuzzleArea();
+        }
+
+        private void InitializePuzzleArea() 
+        {
+            this.BackColor = Color.Gray;
+            this.Text = "Puzzle15";
+        }
+
+        private void InitializeButtons() 
+        {
+            Button button;
+            for (int i = 1; i < 16; i++)
+            {
+                button = new Button();
+                this.Controls.Add(button);
+            }
+        }
+
+
+    }
+}
