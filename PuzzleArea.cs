91ca367d7010000347ec8a36bfdf87998795cffe
Arvils <arvils.apsitis@gmail.com>
2020-07-01 17:23:05 +0300

    Ver 2020.07.01 17:22

---
+++ PuzzleArea.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuzzleFifteen
+{
+    public partial class PuzzleArea : Form
+    {
+        public PuzzleArea()
+        {
+            InitializeComponent();
+            InitializePuzzleArea();
+            InitializeBlocks();
+        }
+
+        private void InitializePuzzleArea()
+        {
+            this.BackColor = Color.DarkSlateBlue;
+            this.Text = "Puzzle15";
+            this.ClientSize = new Size(500, 500);
+        }
+
+        private void InitializeBlocks()
+        {
+            int blockCount = 1;
+            PuzzleBlock block;
+            for (int row = 1; row < 5; row++)
+            {
+                for (int col = 1; col < 5; col++)
+                {
+                    block = new PuzzleBlock()
+                    {
+                        Top = row * 84,
+                        Left = col * 84,
+                        Text = blockCount.ToString(),
+                        Name = "Block" + blockCount.ToString()
+                    };
+
+                    //block.Click += new EventHandler(Block_Click);
+                    block.Click += Block_Click;
+
+                    if (blockCount == 16)
+                    {
+                        block.Name = "EmptyBlock";
+                        block.Text = string.Empty;
+                        block.BackColor = Color.DarkSlateBlue;
+                        block.FlatStyle = FlatStyle.Flat;
+                        block.FlatAppearance.BorderSize = 0;
+                    }
+                    blockCount++;
+                    this.Controls.Add(block);
+                }
+            }
+        }
+
+        private void Block_Click(object sender, EventArgs e)
+        {
+            Button block = (Button)sender;
+            if (IsAdjacent(block))
+            {
+                SwapBlocks(block);
+            }
+        }
+
+        private void SwapBlocks(Button block)
+        {
+            Button emptyBlock = (Button)this.Controls["EmptyBlock"];
+            Point oldLocation = block.Location;
+            block.Location = emptyBlock.Location;
+            emptyBlock.Location = oldLocation;
+        }
+
+        private bool IsAdjacent(Button block)
+        {
+            double a;
+            double b;
+            double c;
+            Button emptyBlock = (Button)this.Controls["EmptyBlock"];
+
+            a = Math.Abs(emptyBlock.Top - block.Top);
+            b = Math.Abs(emptyBlock.Left - block.Left);
+            c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            if (c < 85)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ PuzzleBlock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuzzleFifteen
+{
+    class PuzzleBlock : Button
+    {
+        public PuzzleBlock()
+        {
+            InitializePuzzleBlock();
+        }
+
+        private void InitializePuzzleBlock()
+        {
+            this.BackColor = Color.Orange;
+            this.Height = 80;
+            this.Width = 80;
+            this.Text = "0";
+            this.Font = new Font("Roboto", 18);
+        }
+    }
+}
